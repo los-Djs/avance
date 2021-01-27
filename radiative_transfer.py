bad5f8d79a5e2e75bead1d30bb76c3e5d6484059
Eduardo ceja <lalitoceja@gmail.com>
2021-01-27 11:27:42 -0600

    added tao function and preparations for  simulation

--- radiative_transfer.py
+++ radiative_transfer.py
@@ -4,25 +4,22 @@ import matplotlib.pyplot as plt
 def funcion_fuente(nu, k):
     return nu/k
 
-def calc_k_i():
-    pass
 
-def tao (x, k, i):
-    x/2 
-    pass
+def tao (delta_x, k_i, k_i1 ):
+    return delta_x/2(k_i + k_i1) 
 
 if __name__   == '__main__':
     ## data
     C_s = 7.8e-6
     alpha_line = 0
     nu = 200 #GHz frecuencia de observacion
-    T = 293.15 # grados Kelvin
+    T = [293.15] # grados Kelvin
     theta = 300 * T 
 
-    n = 0 #densidad de la nube, con 0 es que no hay nube
+    ro = [0] #densidad de la nube, con 0 es que no hay nube
     K = 1.380649e-23 # constante de boltzmann joules por kelvin
 
-    P = n*K*T
+    P = ro*K*T
 
     alpha_h20 = (C_s * nu**2 * theta**3 * P ** 2) + alpha_line  ## aqui hay que calcular la opacidad del agua despejando la ecuacion
 
@@ -31,6 +28,10 @@ if __name__   == '__main__':
 
     S_nu = funcion_fuente(nu, k)
 
-    x = 100 #cm
+    delta_x = 100 #cm
+
+    N = 10
+
+    k_n = [nu* alpha_h20 * (T_i/ro_i) for T_i, ro_i in zip(T,ro)]
 
     ## asumiendo que la temperatura es constante y no hay nube de agua
