fixed units and made plot

radiative_transfer_1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 
 def plot(I, delta_x, N):
@@ -6,11 +7,13 @@ def plot(I, delta_x, N):
     acum = 0
     for i in range(N):
         paso.append(acum)
-
-    plt.plot(I)
-    pass
-
-
+        acum += delta_x
+        
+    plt.plot( paso  , I,label = 'Simulacion Transferencia')
+    plt.xlabel("Distancia (m)")
+    plt.title("Transferencia radiativa")
+    plt.show()
+    
 def tao (delta_x, k_i, k_i1 ):
     delta_x = delta_x/2
     k = (k_i + k_i1)
@@ -25,7 +28,7 @@ if __name__   == '__main__':
     N = 10 #numero de iteraciones
     C_s = 7.8e-6  #s^6 kg^2 m
     alpha_line = 0
-    nu = 200 #GHz
+    nu = 200e9 #Hz
     T = [293.15]*N # grados Kelvin equivale a 20 C
     theta = 300/T[0] #adimensional
 
@@ -46,12 +49,13 @@ if __name__   == '__main__':
 
     I = [0.0] #intensidad inicial
     for i in range(1,N):
-        
+        paso = i * delta_x
+        aux1 = I[i-1]* math.exp(-1 * tao(delta_x, k_n[i-1], k_n[i]))
+        aux2 = S_nu * (1 - math.exp(-1 * tao(delta_x, k_n[i-1], k_n[i])))
+
         # S_nu = funcion_fuente(nu, k)
-
-        aux1 = I[i-1]* np.exp(- tao(delta_x, k_n[i-1], k_n[i]))
-        aux2 = S_nu * (1- np.exp(- tao(delta_x, k_n[i-1], k_n[i])))
-
         print(aux1, aux2)
 
         I.append(aux1 + aux2)    
+
+    plot(I, delta_x, N)