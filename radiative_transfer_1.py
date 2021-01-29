8236653ee2a3207d02a6818b0da6554bfa56d268
briankalid <briankalid2000@gmail.com>
2021-01-28 18:54:25 -0600

    pa' revisar jeje

--- radiative_transfer_1.py
+++ radiative_transfer_1.py
@@ -15,6 +15,7 @@ def plot(I, delta_x, N,save=False):
     plt.title("Transferencia radiativa")
     plt.legend()
     plt.yscale('log')
+    plt.xscale('log')
     plt.show()
     if save:
         plt.savefig('Images/radiative_transfer.png')
@@ -26,42 +27,41 @@ def tao (delta_x, k_i, k_i1 ):
     bla = delta_x * k
     return  bla
 
-def funcion_fuente(nu, k):
-    return nu/k
+#def funcion_fuente(nu, k):
+#    return nu/k
+
+def alpha_h20(c_s,nu,theta,p):
+    return (c_s * (nu**2) * (theta**3) * (p**2))
+ 
 
 
 if __name__   == '__main__':
-    N = 10 #numero de iteraciones
+    N = 100000 #numero de iteraciones
     C_s = 7.8e-6  #s^6 kg^2 m
     alpha_line = 0
     nu = 200e9 #Hz
-    T = [293.15]*N # grados Kelvin equivale a 20 C
-    theta = 300/T[0] #adimensional
+    T = 293.15 # grados Kelvin equivale a 20 C
+    theta = 300/T #adimensional
 
-    ro = [0.6] * N # kg/m^3
+    ro = 0.6 # kg/m^3
     K = 1.380649e-23 # constante de boltzmann   m^2 kg s^-2 K^-1
 
-    P = ro[0] * K * T[0]
+    P = ro * K * T
 
-    alpha_h20 = (C_s * (nu**2) * (theta**3) * (P**2)) + alpha_line
+    #alpha_h20 = 
+    k = alpha_h20(C_s,nu,theta,P)
 
-    k = alpha_h20
 
-    S_nu = funcion_fuente(nu, k)
+    delta_x = 100 #m
 
-    delta_x = 1 #m
+    k_n = nu* k * (T/ro)
 
-    k_n = [nu* k * (T_i/ro_i) for T_i, ro_i in zip(T,ro)]
-
-    I = [0.0] #intensidad inicial
+    I = [200.0] #intensidad inicial
     for i in range(1,N):
         paso = i * delta_x
-        aux1 = I[i-1]* math.exp(-1 * tao(delta_x, k_n[i-1], k_n[i]))
-        aux2 = S_nu * (1 - math.exp(-1 * tao(delta_x, k_n[i-1], k_n[i])))
+        aux1 = I[i-1]* math.exp(-1 * tao(delta_x, k_n, k_n))
+        #print(aux1)
 
-        # S_nu = funcion_fuente(nu, k)
-        print(aux1, aux2)
-
-        I.append(aux1 + aux2)    
+        I.append(aux1)    
 
     plot(I, delta_x, N, save=True)
